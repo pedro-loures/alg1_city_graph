a3ec11b48fe3049d0664fa5e52cdef17bf77f8b3
Pedro Loures Alzamora <62044049+pedro-loures@users.noreply.github.com>
2024-11-16 22:14:11 -0300

    organizar file structure

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+src/
+│
+├── main.cpp                // main do programa
+├── graph.hpp               // Header da classe grafos
+├── graph.cpp               // Implementação da classe grafos
+├── cfc.hpp                 // Header da computação dos Componentes Fortementes Conexos
+├── cfc.cpp                 // Implementation da computação dos Componentes Fortemente Conexos
+├── utils.hpp               // Header para funções de utilidades
+└── utils.cpp               // Implementação das funções de utilidades
+*/
+
+// *********** Definindo a capital *********
+
+// *********** /Definindo a capital ********
+// *********** Batalhões *******************
+
+// *********** /Batalhões ******************
+// *********** patrulhamentos **************
+
+// *********** /patrulhamentos *************
+
+
